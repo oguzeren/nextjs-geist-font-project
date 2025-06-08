a35888ae3fbf9d043d9e1411b5a2e3fbb8182738
oguzeren <44902883+oguzeren@users.noreply.github.com>
2025-06-08 07:35:11 +0300

    Add opencart-turkish-version/language/turkish.php 1749357310930

---
+++ opencart-turkish-version/language/turkish.php
@@ -0,0 +1,14 @@
+<?php
+// Turkish language translations
+
+return [
+    'text_home' => 'Ana Sayfa',
+    'text_welcome' => 'Hoşgeldiniz',
+    'button_cart' => 'Sepete Ekle',
+    'button_checkout' => 'Ödeme',
+    'text_payment' => 'Ödeme Yöntemleri',
+    'text_shipping' => 'Kargo Yöntemleri',
+    'error_payment' => 'Ödeme sırasında hata oluştu.',
+    // Add more translations as needed
+];
+?>
