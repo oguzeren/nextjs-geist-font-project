8b8a7473469c2be6e4829b35a95dd27b349ec8c6
oguzeren <44902883+oguzeren@users.noreply.github.com>
2025-06-08 07:35:14 +0300

    Add opencart-turkish-version/config.php 1749357314940

---
+++ opencart-turkish-version/config.php
@@ -0,0 +1,10 @@
+<?php
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'opencart_turkish');
+
+// Base URL
+define('BASE_URL', 'http://localhost/opencart-turkish-version/');
+?>
